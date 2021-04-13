624c982caa1bd8caaa5bfee6eb4fa557329dc866
Dmitrii Tkachev <55581820+Dmitrii-Weaver@users.noreply.github.com>
2021-04-13 09:45:28 +0300

    login kinda works

---
+++ app/src/main/java/pet/plants/LoggedInActivity.kt
@@ -0,0 +1,16 @@
+package pet.plants
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+
+class LoggedInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged)
+
+
+    }
+}
