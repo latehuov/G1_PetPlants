2a4dbf0127d20f454b2f7b8ea9b97657b207aa53
Nabeel <nabeelbaig962@gmail.com>
2021-04-26 11:13:05 +0300

    Attempting implement the working code by first adding the libraries.

--- app/src/main/java/pet/plants/ui/payment/payment.kt
+++ app/src/main/java/pet/plants/ui/payment/payment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.telecom.Call
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import pet.plants.R
 
 // TODO: Rename parameter arguments, choose names that match
